9607f208edaf86379b75f7511ccc123b6337354c
Viwe Nkepu <viwe.nkepu@hotmail.com>
2025-01-19 15:38:42 +0200

    move files

--- src/Blazor/Program.cs
+++ src/Blazor/Program.cs
@@ -1,6 +1,6 @@
-using Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
